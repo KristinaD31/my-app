8ebdd34695cf23a53a049958086ebf192827a3dc
Kristina <112591859+KristinaD31@users.noreply.github.com>
2022-10-19 18:39:19 -0400

    clock component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = 'Kristina';
+  const element1 = <h1>Hello, {name}</h1>;
+
+  function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+  
+  const user = {
+    firstName: 'Kristina',
+    lastName: 'Delia'
+  };
+  
+  const element2 = (
+    <h1>
+      Hello, {formatName(user)}!
+    </h1>
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Clock">
+     {element2}
     </div>
   );
 }
